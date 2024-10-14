5b1937d6ea076570eff753a1e95c501ea57e609e
Verónica Rosales <veronica.rosales@ucuenca.edu.ec>
2024-10-14 17:02:04 -0500

    modificaciones

--- arrays.js
+++
@@ -1,8 +0,0 @@
-let frutas = ["manzana", "pera", "naranja"];
-let precios = [24, 35, 999, 34, 234];
-
-for (let i = 0; i < frutas.length; i += 1) {
-  console.log(frutas[i]);
-}
-
-console.log(precios);
--- sketch.js
+++ sketch.js
@@ -1,55 +1,23 @@
-let bgColor;
-let pelotas = [];
+let particulas = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bgColor = color(155, 0, 255);
-  for (let i = 0; i < 100; i++) {
-    let nuevaPelota = new Pelota();
-    pelotas.push(nuevaPelota);
-  }
 }
 
 function draw() {
-  background(bgColor);
-  for (let i = 0; i < 100; i++) {
-    pelotas[i].update();
-    pelotas[i].display();
+  background(20, 10);
+  let nuevaParticula = new Particula(mouseX, mouseY);
+  particulas.push(nuevaParticula);
+
+  for (let i = 0; i < particulas.length; i++) {
+    particulas[i].update();
+    particulas[i].display();
   }
-}
-
-function changeBackgroundColor() {
-  bgColor = color(random(255), random(255), random(255));
-}
-
-class Pelota {
-  //Plano de instrucciones para crear una pelota
-  constructor() {
-    this.rad = ceil(random(5, 30));
-    this.diam = this.rad * 2;
-    this.posX = random(this.rad, width - this.rad);
-    this.posY = random(this.rad, height - this.rad);
-    this.velX = random(-10, 10);
-    this.velY = random(-10, 10);
-    console.log("estoy vivaaa!");
-  }
-  //método update
-  update() {
-    this.posX += this.velX;
-    this.posY += this.velY;
-
-    if (this.posX > width - this.rad || this.posX < this.rad) {
-      this.velX *= -1;
-    }
-
-    if (this.posY > height - this.rad || this.posY < this.rad) {
-      this.velY *= -1;
+  for (let i = 0; i < particulas.length; i++) {
+    if (particulas[i].estaViva) {
+    } else {
+      particulas.splice(i, 1);
     }
   }
-
-  display() {
-    fill(255);
-    noStroke();
-    circle(this.posX, this.posY, this.diam);
-  }
+  console.log(particulas.length);
 }
