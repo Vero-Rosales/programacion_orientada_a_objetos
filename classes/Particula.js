fcd80c23d4cb93a42dae50bf263353a5fc4c1b4e
Verónica Rosales <veronica.rosales@ucuenca.edu.ec>
2024-10-17 11:44:56 -0500

    modificación propia

--- classes/Particula.js
+++ classes/Particula.js
@@ -7,7 +7,10 @@ class Particula {
     // tVida es el tiempo de vida
     this.tVida = round(random(10, 200));
     this.tamaño = 50;
+    // Asignamos un color aleatorio a cada partícula
+    this.color = color(random(255), random(255), random(255));
   }
+
   update() {
     this.tVida -= 1;
     if (this.tVida <= 0) {
@@ -18,9 +21,18 @@ class Particula {
     this.posx += random(-10, 10);
     this.tamaño -= 0.5;
   }
+
   display() {
-    fill(255);
+    // Usamos el color de la partícula
+    fill(this.color);
     noStroke();
-    circle(this.posx, this.posy, this.tamaño);
+
+    // Dibujar el rombo
+    beginShape();
+    vertex(this.posx, this.posy - this.tamaño / 2); // vértice superior
+    vertex(this.posx + this.tamaño / 2, this.posy); // vértice derecho
+    vertex(this.posx, this.posy + this.tamaño / 2); // vértice inferior
+    vertex(this.posx - this.tamaño / 2, this.posy); // vértice izquierdo
+    endShape(CLOSE);
   }
 }
