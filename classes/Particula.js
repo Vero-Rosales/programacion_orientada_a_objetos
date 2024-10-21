cc97af1cacc5b4afc1e5a57e5f09d842c149c918
Verónica Rosales <veronica.rosales@ucuenca.edu.ec>
2024-10-21 13:32:38 -0500

    modificaciones

--- classes/Particula.js
+++ classes/Particula.js
@@ -1,4 +1,4 @@
-class Particula {
+/*class Particula {
   // este método se ejecuta automáticamente
   constructor(_x, _y) {
     this.posx = _x;
@@ -52,5 +52,42 @@ class Particula {
       rotate(angulo);
     }
     pop();
+  }
+}*/
+
+class Particula {
+  // este método se ecuta automáticamente
+  constructor(_x, _y) {
+    this.posx = _x;
+    this.posy = _y;
+    this.estaViva = true;
+    // tVida es el tiempo que tiene de vida
+    this.tVida = round(random(10, 200));
+    this.tamano = 1;
+    this.opacidad = 100;
+  }
+  update() {
+    // this.tVida -= 1;
+    // if (this.tVida <= 0) {
+    // 	this.estaViva = false;
+    // 	return;
+    // }
+
+    if (this.posy < this.tamano / 2) {
+      this.estaViva = false;
+      return;
+    }
+
+    this.opacidad -= 0.5;
+
+    this.posy -= 2;
+    this.posx += random(-10, 10);
+    this.tamano += 0.5;
+  }
+  display() {
+    fill(255, this.opacidad);
+    noStroke();
+    circle(this.posx, this.posy, this.tamano);
+    circle();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let particulas = [];
+/*let particulas = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -19,5 +19,59 @@ function draw() {
       particulas.splice(i, 1);
     }
   }
+  console.log(particulas.length);
+}*/
+
+let particulas = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(20);
+  let nuevaParticula = new Particula(mouseX, mouseY);
+  particulas.push(nuevaParticula);
+
+  for (let i = 0; i < particulas.length; i++) {
+    particulas[i].update();
+    particulas[i].display();
+  }
+
+  /**
+   * Esta es una forma de borrar las partículas muertas utilizando
+   * un for loop que navega todas las partículas y se pergunta por
+   * la varible boleana estaViva.
+   *
+   * Utiliza la función slipice para borrar la partícula específica
+   * mediante su Indice
+   *
+   */
+
+  // for (let i = 0; i < particulas.length; i++) {
+  // 	if (!particulas[i].estaViva) {
+  // 		particulas.splice(i, 1);
+  // 	}
+  // }
+
+  /**
+   * Esta es la forma moderna de resolverlo
+   */
+
+  particulas = particulas.filter((pelota) => pelota.estaViva);
+
+  noFill();
+  stroke(252, 99, 145);
+  strokeWeight(1);
+
+  for (let i = 0; i < particulas.length - 1; i++) {
+    line(
+      particulas[i].posx,
+      particulas[i].posy,
+      particulas[i + 1].posx,
+      particulas[i + 1].posy
+    );
+  }
+
   console.log(particulas.length);
 }
